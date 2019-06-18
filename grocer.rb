973d107698f9ab37398eca38fc0345686fe67357
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-18 14:37:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,9 +34,23 @@ end
 
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |name, properties|
+    if properties[:clearance]
+      updated_price = properties[:price] * 0.80
+      properties[:price] = updated_price.round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(couponed_cart)
+  total = 0
+  final_cart.each do |name, properties|
+    total += properties[:price] * properties[:count]
+  end
+  total = total * 0.9 if total > 100
+  total
 end
